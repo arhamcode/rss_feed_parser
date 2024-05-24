e1d91212c4bc21e25aa2ac09b2f9c0d9feb9b7de
Arham Anwar <arham.code@gmail.com>
2024-05-24 14:09:35 +0800

    Add documentation for atom category

--- lib/domain/atom_category.dart
+++ lib/domain/atom_category.dart
@@ -1,8 +1,19 @@
 import 'package:xml/xml.dart';
 
+/// Conveys information about a category associated with an entry or feed.
+/// This specification assigns no meaning to the content (if any) of this
+/// element.
 class AtomCategory {
+  /// a string that identifies the category to  which the entry or feed belongs.
   final String? term;
+
+  /// an IRI that identifies a categorization scheme.
   final String? scheme;
+
+  /// provides a human-readable label for display in end-user applications.
+  /// The content of the "label" attribute is Language-Sensitive.
+  /// Entities such as "&amp;" and "&lt;" represent their corresponding
+  /// characters ("&" and "<", respectively), not markup.
   final String? label;
 
   const AtomCategory(this.term, this.scheme, this.label);
